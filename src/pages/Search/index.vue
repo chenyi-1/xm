cc081a17d252716c561a3e0161884324cb7ed367
chenyi-1 <2030069829@qq.com>
2020-08-11 22:37:02 +0800

    firstCommit

---
+++ src/pages/Search/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Search<br>
+     params参数:{{$route.params.keyword}}
+    params:{{keyword}}
+    query参数:{{$route.query.keyword}}
+    query:{{keyword2}}
+    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props:['keyword','keyword2','name']
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
